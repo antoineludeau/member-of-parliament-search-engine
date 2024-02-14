cf3daaab76124084ea67e8a7c501634178531463
antoineludeau <52679050+antoineludeau@users.noreply.github.com>
2024-02-14 10:02:31 +0100

    Added remote pattern for nosdeputes.fr photos

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.nosdeputes.fr',
+        port: '',
+        pathname: '/depute/photo/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
