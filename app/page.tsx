082f664265eec9d672abc4ef4fb8d9c63357d51a
antoineludeau <52679050+antoineludeau@users.noreply.github.com>
2024-11-28 16:17:57 +0100

    Modified nosdeputes url data source

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const Home = async (): Promise<JSX.Element> => {
 }
 
 async function getData() {
-  const res = await fetch("https://www.nosdeputes.fr/deputes/enmandat/json", {
+  const res = await fetch("https://www.nosdeputes.fr/deputes/json", {
     // data is cached server side for 24 hours.
     // This means that the data is only fetched once every 24 hours for all users requesting the page.
     next: { revalidate: 86400 },
@@ -18,7 +18,9 @@ async function getData() {
     throw new Error("Failed to fetch data");
   }
 
+  
   const responseJson: APIParliamentResponse = await res.json();
+  console.log('responseJson', responseJson);
   const memberList = responseJson.deputes.map((member) => member.depute);
   return memberList;
 }
